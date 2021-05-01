63a4ac0cb3d28a6f8104e07f31b81e18b08aae04
Saikat30101997 <saikat30101997@gmail.com>
2021-05-01 12:42:51 -0700

    ADDED

---
+++ FirstDemo/FirstDemo/Controllers/DashboardController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstDemo.Models;
+
+namespace FirstDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public string s;
+        public IActionResult Summary()
+        {
+            var model = new SummaryModel();
+            return View(model);
+        }
+        public ViewResult StudentBranch(string name)    
+        {
+            //s = name;
+            //StudentInfo studentinfo = new StudentInfo();
+            //var x = studentinfo.GetStudents();
+            //Student student = new Student();
+            //var res = (from std in studentinfo.GetStudents() where std.Name == name select std).ToList();
+            //var ans = new
+            //{
+            //    Reg = res.Select(x=>x.RegNo),
+            //    Nam = res.Select(std=>std.Name),
+
+            //};
+            //return View(ans);
+            StudentInfo model = new StudentInfo();
+            return View(model.GetStudents());
+            //StudentInfo studentInfo = new StudentInfo();
+
+
+
+
+
+            //var x = studentInfo.GetStudents();
+
+            //var res = (from std in studentInfo.GetStudents() where std.Name == name select std).ToList();
+
+            //ViewData["Student"] = res;
+            //return View();
+
+
+
+        }
+    }
+}
---
+++ FirstDemo/FirstDemo/Models/StudentBranch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstDemo.Controllers;
+using System.Reflection;
+namespace FirstDemo.Models
+{
+    public class Student
+    {
+        public int RegNo { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+
+    public class StudentInfo
+    {
+       
+
+        public List<Student>GetStudents()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){RegNo = 1,Name = "Saikat"},
+                new Student(){RegNo=2,Name="Sohel"}
+            };
+            return students;
+        }
+
+      
+        
+
+  
+    }
+}
