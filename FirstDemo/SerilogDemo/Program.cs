ac5cf912fa14b22b86886e6d11154982a0deb863
Saikat30101997 <saikat30101997@gmail.com>
2021-05-12 01:17:44 +0600

    added databaseserilog

---
+++ FirstDemo/SerilogDemo/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configbuilder = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json", false)
+                                .AddEnvironmentVariables()
+                                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                         .MinimumLevel.Debug()
+                         .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                         .Enrich.FromLogContext()
+                         .ReadFrom.Configuration(configbuilder)
+                         .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application Starting Failed");
+
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+           
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http//*:80");
+                });
+    }
+}
