0118b08d266c560339bef68ea96707e49b79534d
Saikat30101997 <saikat30101997@gmail.com>
2021-05-17 15:51:41 +0600

    added htmltaghelper

---
+++ Assighnment1/Task2/ClassWithNonPublicMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class ClassWithNonPublicMethods
+    {
+        private void PrivateMethod(string value)
+        {
+            Console.WriteLine(value);
+        }
+        protected void ProtectedMethod(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
