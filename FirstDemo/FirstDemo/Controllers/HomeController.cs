29735c45a18a3a764a01e3fb49a249d068bee822
Saikat30101997 <saikat30101997@gmail.com>
2021-05-23 10:49:17 +0600

    bootstrap

--- FirstDemo/FirstDemo/Controllers/HomeController.cs
+++ FirstDemo/FirstDemo/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace FirstDemo.Controllers
         {
             return View();
         }
+        public IActionResult Bootstrap()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
