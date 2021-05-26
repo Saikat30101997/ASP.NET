2dee9873de518af84c2208d0c42b518051ac85d0
Saikat30101997 <saikat30101997@gmail.com>
2021-05-26 18:32:05 +0600

    added Typescripts

--- FirstDemo/FirstDemo/wwwroot/ts/demo.js
+++
@@ -1,6 +0,0 @@
-var decimal = 6;
-var hex = 0xf00d;
-var binary = 10;
-var octal = 484;
-
-//# sourceMappingURL=demo.js.map
--- FirstDemo/FirstDemo/wwwroot/ts/out/demo.js
+++ FirstDemo/FirstDemo/wwwroot/ts/out/demo.js
@@ -1,16 +1,14 @@
-﻿/*let list: number[] = [1, 2, 3];
+/*let list: number[] = [1, 2, 3];
 
 for (let i = 0; i < list.length; i++) {
     console.log(list[i]);
 }
 
 */
-
 /*let x: [string, number];
 x = ["Saikat", 10];
 console.log(x[0].substring(1));
 */
-
 /*enum Color {
     Red,
     Green,
@@ -20,7 +18,6 @@ console.log(x[0].substring(1));
 let c: Color = Color.Red;
 console.log(c) //value print korbee 0 theke index start
 */
-
 /*enum Color {
     Red=1,
     Green,
@@ -42,7 +39,7 @@ console.log(colorName); // 2 number index e kn color ache tar name ta print korb
 /*let notsure: any = 4;
 //notsure = false;
 //notsure.IfItExists(); // compile time error nai bt run time error dekhabe karon typescript+javascript extension method belong koree . r ifitexists() nam e func create kora nai ba builtin e nai
-console.log(notsure.toFixed());  //eta set precision type niye kaj koree 
+console.log(notsure.toFixed());  //eta set precision type niye kaj koree
 */
 /* // void datatype
 function warnuser(): void {
@@ -51,19 +48,15 @@ function warnuser(): void {
 warnuser();
 
 */
-
 // never datatype
-
 // Function returning never must have unreachable end point
-/*function error(message: string): never {
-    throw new Error(message)
-} 
-  // Inferred return type is never
+function error(message) {
+    throw new Error(message);
+}
+// Inferred return type is never
 function fail() {
     return error("Something failed");
 }
-
-let err: string = fail();
+var err = fail();
 console.log(err);
-
-*/
+//# sourceMappingURL=demo.js.map
