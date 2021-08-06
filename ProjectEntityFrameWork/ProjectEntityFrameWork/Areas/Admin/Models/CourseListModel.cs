a98275762d52c61d73056c5c27e41b68d883df32
Saikat30101997 <saikat30101997@gmail.com>
2021-08-07 01:01:46 +0600

    added 1:00:10

--- ProjectEntityFrameWork/ProjectEntityFrameWork/Areas/Admin/Models/CourseListModel.cs
+++ ProjectEntityFrameWork/ProjectEntityFrameWork/Areas/Admin/Models/CourseListModel.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Microsoft.AspNetCore.Http;
 using ProjectEntityFrameWork.Models;
 using ProjectEntityFrameWork.Training.BusinessObjects;
 using ProjectEntityFrameWork.Training.Services;
@@ -11,21 +12,24 @@ namespace ProjectEntityFrameWork.Areas.Admin.Models
 {
     public class CourseListModel
     {
-        private ICourseService _courseService;
-       
+        private readonly ICourseService _courseService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         public CourseListModel()
         {
             _courseService = Startup.AutofacContainer.Resolve<ICourseService>();
+            _httpContextAccessor = Startup.AutofacContainer.Resolve<IHttpContextAccessor>();
         }
 
-        public CourseListModel(ICourseService courseService)
+        public CourseListModel(ICourseService courseService,IHttpContextAccessor httpContextAccessor)
         {
             _courseService = courseService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
 
         internal object GetCourses(DataTablesAjaxRequestModel tableModel) //tuple return korbe courseservice er
         {
+        
             var data = _courseService.GetCourses(
                tableModel.PageIndex,
                tableModel.PageSize,
