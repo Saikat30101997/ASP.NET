4f6caed08de833f6ba446602277f4b21324da58a
Saikat30101997 <saikat30101997@gmail.com>
2021-08-26 18:19:39 +0600

    added bootstrap theme

--- InventorySystem/InventorySystem.Web/Startup.cs
+++ InventorySystem/InventorySystem.Web/Startup.cs
@@ -97,6 +97,10 @@ namespace InventorySystem.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
