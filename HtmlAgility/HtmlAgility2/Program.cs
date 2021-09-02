9c94be037bbe50bdd9a27117e1f7ad8ea4bf2654
Saikat30101997 <saikat30101997@gmail.com>
2021-09-02 15:25:59 +0600

    added

--- HtmlAgility/HtmlAgility2/Program.cs
+++ HtmlAgility/HtmlAgility2/Program.cs
@@ -12,11 +12,14 @@ namespace HtmlAgility2
         {
             HtmlWeb web = new HtmlWeb();
             HtmlDocument document = web.Load("https://www.dse.com.bd/latest_share_price_scroll_l.php");
-          
+
             var nodes = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table[contains(@class, " +
                 "'table table-bordered background-white shares-table fixedHeader')]");
-           
-            
+
+            var item = nodes.FirstOrDefault().SelectSingleNode("//span/span").InnerText;
+            Console.WriteLine(item);
+
+
         }
     }
 }
